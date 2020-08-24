1c7a8c045cee7cae74fd29d5356850fd5427b39e
smallsunjj <jyang57@stevens.edu>
2020-08-24 14:08:29 -0400

    first try on task1

--- src/main/java/com/yourorganization/maven_sample/JavaParserTask1.java
+++ src/main/java/com/yourorganization/maven_sample/JavaParserTask1.java
@@ -25,7 +25,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JavaParserTask {
+public class JavaParserTask1 {
   private static CombinedTypeSolver combinedSolver;
   private static final String src_dir = "src/main/resources/avro/lang/java/";
   private static final String file_name_1 = "avro/src/test/java/org/apache/avro/io/parsing/TestResolvingGrammarGenerator.java";
@@ -75,9 +75,55 @@ public class JavaParserTask {
     return list;
   }
 
+/*  private static class MethodNameCollector extends VoidVisitorAdapter<List<String>> {
+    @Override
+    public void visit(MethodDeclaration md, List<String> collector) {
+      super.visit(md, collector);
+      collector.add(md.getNameAsString());
+    }
+  }*/
+
+  private static class MethodDeclarationCollector extends VoidVisitorAdapter<List<MethodDeclaration>> {
+    @Override
+    public void visit(MethodDeclaration md, List<MethodDeclaration> collector) {
+      super.visit(md, collector);
+      collector.add(md);
+    }
+  }
+
+  private static class AssertCollector extends VoidVisitorAdapter<List<String>> {
+    @Override
+    public void visit(AssertStmt as, List<String> collector) {
+      super.visit(as, collector);
+      collector.add(as.toString());
+    }
+  }
+
+  private static class MethodCallExpCollector extends VoidVisitorAdapter<List<MethodCallExpr>> {
+    @Override
+    public void visit(MethodCallExpr mce, List<MethodCallExpr> collector) {
+      super.visit(mce, collector);
+      //System.out.println(mce);
+      collector.add(mce);
+    }
+  }
+  private static class MethodCallExpressionVisitor extends VoidVisitorAdapter<Void> {
+    @Override
+    public void visit(MethodCallExpr mce, Void arg) {
+      System.out.println("======================");
+      System.out.println("Method Call Expression: \n" + mce);
+      try {
+        mce.resolve();
+      } catch (Exception e) {
+        System.err.println(e);
+      }
+
+    }
+  }
+
   public static void main(String[] args) throws IOException {
     // JavaSymbolSolver configuration
-    JavaParserTask jp = new JavaParserTask();
+    JavaParserTask1 jp = new JavaParserTask1();
     jp.initCombinedSolver();
     ParserConfiguration parserConfiguration = new ParserConfiguration();
     JavaParser jvp = new JavaParser(parserConfiguration);
@@ -87,6 +133,49 @@ public class JavaParserTask {
     CompilationUnit cu1 = jvp.parse(new File(src_dir + file_name_1)).getResult().get();
     CompilationUnit cu2 = jvp.parse(new File(src_dir + file_name_2)).getResult().get();
     CompilationUnit cu3 = jvp.parse(new File(src_dir + file_name_3)).getResult().get();
+
+
+
+    List<MethodDeclaration> methodModules = new ArrayList<>();
+    VoidVisitor<List<MethodDeclaration>> MethodDeclarationCollector = new MethodDeclarationCollector();
+    VoidVisitor<List<MethodCallExpr>> MethodCallExpCollector = new MethodCallExpCollector();
+    VoidVisitor<List<String>> AssertCollector = new AssertCollector();
+    MethodDeclarationCollector.visit(cu1, methodModules);
+
+    VoidVisitor<Void> MethodCallExpressionVisitor = new MethodCallExpressionVisitor();
+    List<String> res = new ArrayList<>();
+    List<MethodCallExpr> assertList = new ArrayList<>();
+    List<MethodCallExpr> finalMethodList = new ArrayList<>();
+    //NodeList<Expression> expressionList = new NodeList<>();
+    for (MethodDeclaration md : methodModules) {
+
+      if (md.isAnnotationPresent("Test")) {
+/*        res.add(md.getNameAsString());
+        md.accept(AssertCollector, assertList); */
+        //System.out.println(md);
+        MethodCallExpCollector.visit(md, assertList);
+        //md.accept(MethodCallExpCollector, assertList);
+        for (MethodCallExpr mce : assertList) {
+          System.out.println(mce);
+          if (mce.getNameAsString().contains("assert")) {
+            //System.out.println(mce);
+            for (Expression e : mce.getArguments()) {
+              //List<MethodCallExpr> finalMethodList = new ArrayList<>();
+              e.accept(MethodCallExpCollector, finalMethodList);
+
+            }
+          }
+        }
+      }
+    }
+/*    res.forEach(n -> System.out.println("Method Name Collected: " + n));
+    assertList.forEach(n -> System.out.println("Method Name Collected: " + n));*/
+    //finalMethodList.forEach(n -> System.out.println(n.getName().));
+    for (MethodCallExpr n : finalMethodList) {
+      //System.out.println(n);
+      //System.out.println(n.resolve().getQualifiedName());
+    }
+    //finalMethodList.forEach(n -> System.out.println("Method Name Collected: " + n.resolve().getQualifiedName()));
 
   }
 }
